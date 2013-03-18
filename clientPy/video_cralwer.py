a825e34de5c57b2e91c49be8bd623effd504a50e
Woonhyuk Baek <bwhyuk@gmail.com>
2013-03-18 22:21:26 +0900

    patch main process

--- clientPy/video_cralwer.py
+++ clientPy/video_cralwer.py
@@ -4,18 +4,23 @@ from JarvisPy.crawler.youtube import Youtube
 from JarvisPy.crawler.tvpot import Tvpot
 
 if __name__ == '__main__':
-    if len(sys.argv) != 3:
-	print 'Usage : python ' + sys.argv[0] + ' url_path("http://www.youtube.com/watch?v=mV6cvBorTfg") output_file.mp4'
+    if len(sys.argv) < 2:
+	print 'Usage : python ' + sys.argv[0] + ' url_path("http://www.youtube.com/watch?v=mV6cvBorTfg") [output_filename(optional)]'
 	exit(0)
 
     url = sys.argv[1]
-    filename = sys.argv[2]
     crawler = ''
     if 'youtu' in url:
 	crawler = Youtube(url)
     elif 'daum' in url:
 	crawler = Tvpot(url)
     video = crawler.downloadVideo()
+
+    filename = ''
+    if len(sys.argv) == 3:
+	filename = sys.argv[2]
+    else:
+	filename = crawler.getVideoId() + '.flv'
 
     fp = open(filename, 'w')
     fp.write(video)
