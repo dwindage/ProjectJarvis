187d72d5ffc8e816cbb75f9e389bd4020b5b6406
Woonhyuk Baek <dwindage@gmail.com>
2013-05-05 16:53:38 +0900

    fix not downloaded video

--- youtube.py
+++ youtube.py
@@ -9,9 +9,10 @@ if __name__ == '__main__':
     youtube = Youtube(sys.argv[1])
     video = youtube.downloadVideo()
 
-    fp = open('/home/pi/videos/others/' + youtube.getVideoId() + ".mp4", 'w')
-    fp.write(video)
-    fp.close()
+	if len(video) > 0:
+		fp = open('/home/pi/videos/others/' + youtube.getVideoId() + ".mp4", 'w')
+		fp.write(video)
+		fp.close()
 
     print 'done.'
 
