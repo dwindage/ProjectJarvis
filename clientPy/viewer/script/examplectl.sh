25b86ce0aa058bac97f3090aeb2bd2948e19076d
Woonhyuk Baek <dwindage@gmail.com>
2013-05-13 10:22:46 +0900

    add viewer template

---
+++ clientPy/viewer/script/examplectl.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+PYTHON_EXECUTE='python'
+
+BASE_PATH=`pwd`/../
+EXECUTE_FILE=listener.py
+PORT=2901
+LOG_FILE=$BASE_PATH/logs/log.stdout
+WORKING_EXECUTE=$BASE_PATH/script/$EXECUTE_FILE
+WORKING_HOME=`dirname $WORKING_EXECUTE`
+cd $WORKING_HOME
+
+
+echo "#############################################"
+echo " execute working home : $WORKING_HOME"
+echo "#############################################"
+
+case $1 in
+	'start')
+		PID=`ps x | grep $EXECUTE_FILE | grep -v grep | awk '{print $1}'`
+		if [ $PID ]; then
+			kill -15 $PID
+			if [ $? == 0 ]; then
+				echo "listener is runiing with pid $PID"
+				echo "kill and restart process"
+			fi
+		fi
+		echo "======================================" >> $LOG_FILE
+		$PYTHON_EXECUTE $WORKING_EXECUTE $PORT >> $LOG_FILE &
+		PID=$!
+		echo "listener started with pid $PID"
+		;;
+	'stop')
+		STARTED=0
+		PID=`ps x | grep $EXECUTE_FILE | grep -v grep | awk '{print $1}'`
+		if [ $PID ]; then
+			kill $PID
+			if [ $? == 0 ]; then
+				STARTED=1
+			fi
+		fi
+		if [ $STARTED == 0 ]; then
+			echo "listener is not running"
+			exit 1
+		fi
+		kill -15 $PID
+		echo "listner width pid $PID stopped"
+		;;
+	*)
+		echo "$0 [ start | stop ]"
+		;;
+esac
---
+++ clientPy/viewer/script/listener.py
@@ -0,0 +1,154 @@
+#usr/bin/python
+# -*- coding: utf-8 -*-
+# @author Woonhyuk Baek
+
+import os, sys, socket 
+import time, uuid
+import logging
+from string import Template
+
+import twisted
+from twisted.web import server, resource, http
+from twisted.internet import reactor, threads, defer
+
+PROJECT_NAME = 'SAMPLE'
+BASE_PATH = os.path.dirname(os.path.abspath(__file__))
+HTML_FRAME_TEMPLATE = BASE_PATH + 'template/frame.html'
+HTML_DATA_TEMPLATE = BASE_PATH + 'template/data.html'
+HTML_QUERY_TEMPLATE = BASE_PATH + 'template/query.html'
+
+UPLAOD_FILE_PATH = BASE_PATH + 'upload/'
+UPLOAD_FILE_URL = 'HTTP_ALIAS_PTH/'
+
+def getReqArg(args, key, default=''):
+    value = default
+    try:
+        if args.has_key(key):
+            if len(args[key][0]) > 0:
+                value = args[key][0]
+    except Exception, e:
+        print '[ERRO]', '[getReqArg]', str(e)
+    return value
+
+def index(req):
+    queryTemplate = Template(open(HTML_QUERY_TEMPLATE).read())
+    queryBindingData = {'QUERY':'', 'IMAGE':'', 'DESCRIPTION':''}
+    query = queryTemplate.substitute(queryBindingData)
+
+    frameTemplate = Template(open(HTML_FRAME_TEMPLATE).read())
+    frameBindingData = {'PROJECT_NAME':PROJECT_NAME, 'QUERY':query, 'BODY':''}
+    resultHtml = frameTemplate.substitute(frameBindingData)
+
+    req.setHeader('Content-type', 'text/html')
+    req.write(resultHtml)
+    req.finish()
+    return
+
+def search(req):
+    try:
+        args = req.args
+        imageUrl = getReqArg(args, "q", "")
+        filestream = getReqArg(args, "f", "")
+        queryImage = imageUrl
+
+        # if file uploaed
+        if len(imageUrl) == 0 and len(filestream) > 0:
+            # save file
+            filename = '%s_%s.jpg' % (time.strftime("%H-%M-%S", time.localtime()), uuid.uuid4())
+            filepath = UPLAOD_FILE_PATH 
+            try:
+                if not os.path.exists(filepath):
+                    os.makedirs(filepath)
+                of = file(filepath + filename, 'wb')
+                of.write(filestream)
+                of.close()
+
+                fileurl = UPLOAD_FILE_URL + filename
+                queryImage = fileurl
+            except Exception, e:
+                print '[ERRO]', '[upload]', str(e)
+            
+        queryTemplate = Template(open(HTML_QUERY_TEMPLATE).read())
+        queryBindingData = {'QUERY':queryImage, 'IMAGE':queryImage, 'DESCRIPTION':''}
+        query = queryTemplate.substitute(queryBindingData)
+
+        dataTemplate = Template(open(HTML_DATA_TEMPLATE).read())
+        data = ''
+        if len(queryImage) > 1: # is valid query
+            # call search engine
+            # ...
+
+            # result
+            resultList = [
+                {'image':'http://10.33.37.185/flickr/Starbucks/7851954064_7299785025_b.jpg'},
+                {'image':'http://10.33.37.185/flickr/Starbucks/7560466908_2be71fa4ce_b.jpg'},
+                {'image':'http://10.33.37.185/flickr/Starbucks/7875493092_45e0a116e6_b.jpg'},
+                {'image':'http://10.33.37.185/flickr/Starbucks/7910756016_2f58993ab1_b.jpg'},
+                {'image':'http://10.33.37.185/flickr/Starbucks/7872406120_7073fdd596_b.jpg'},
+                {'image':'http://10.33.37.185/flickr/Starbucks/7822293938_2f0863ee1a_b.jpg'},
+                ]
+
+            for idx, result in enumerate(resultList):
+                dataBindingData = {'RANK':count, 'SCORE':0, 'IMAGE':result['image']}
+                data += dataTemplate.substitute(dataBindingData)
+            if count == 0:
+                data = '<div class="alert"><strong>Warning!</strong> No Results!!</div>'
+
+        frameTemplate = Template(open(HTML_FRAME_TEMPLATE).read())
+        frameBindingData = {'PROJECT_NAME':PROJECT_NAME, 'QUERY':query, 'BODY':data}
+        resultHtml = frameTemplate.substitute(frameBindingData)
+
+        req.setHeader('Content-type', 'text/html')
+        req.write(resultHtml)
+        req.finish()
+    except Exception, e:
+        print '[ERRO]', '[search]', str(e)
+    return
+
+class HandleRequest(http.Request):
+    pageHandlers = {
+        '/':index,
+        '/search':search,
+        }
+
+    def process(self):
+        if self.pageHandlers.has_key(self.path):
+            # is valid request
+            print '[INFO]', '[REQUEST]',
+            print self.getClientIP() + ' ' + self.method + ' ' + self.uri
+            print '[INFO]', '[REQUEST]', '[PARAM] ' + str(self.args)[:200]
+
+            handler = self.pageHandlers[self.path]
+            d = threads.deferToThread(handler, self)
+        else:
+            self.setResponseCode(http.NOT_FOUND)
+            self.setHeader('Content-type', 'text/html')
+            self.write('<H1>Not Found</H1>Sorry, no such page.')
+            self.finish()
+
+class MyHttp(http.HTTPChannel):
+    requestFactory = HandleRequest
+
+class MyHttpFactory(http.HTTPFactory):
+    protocol = MyHttp
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print sys.stderr, 'Usage : ' + sys.argv[0] + ' PORT'
+        exit()
+
+    port = int(sys.argv[1])
+    localIP = socket.gethostbyname(socket.gethostname())
+
+    print '[INFO]', '[DAEMON]', ':::::::::::::::::::::::::::::::::::'
+    print '[INFO]', '[DAEMON]', 'START listener - ' + str(localIP) + ':' + str(port)
+    print '[INFO]', '[DAEMON]', ':::::::::::::::::::::::::::::::::::'
+
+    reactor.suggestThreadPoolSize(16)
+    reactor.listenTCP(port, MyHttpFactory())
+    reactor.run()
+
+    print '[INFO]', '[DAEMON]', ':::::::::::::::::::::::::::::::::::'
+    print '[INFO]', '[DAEMOM]', 'STOP listener'
+    print '[INFO]', '[DAEMON]', ':::::::::::::::::::::::::::::::::::'
+ 
